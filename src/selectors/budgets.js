360cae95649e2703fa1d006b7e9b0e76f4b409d3
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-25 20:43:46 +0200

    fix err

--- src/selectors/budgets.js
+++ src/selectors/budgets.js
@@ -3,9 +3,9 @@ export default (budgets, {text, sortBy, startDate, endDate}) => {
     return budgets.filter((budget) => {
       const startDateMatch = typeof startDate !== 'number' || budget.createdAt >= startDate;
       const endDateMatch = typeof endDate !== 'number' || budget.createdAt <= endDate;
-      const textMatch = budget.description.toLowerCase().includes(text.toLowerCase());
+//      const textMatch = budget.description.toLowerCase().includes(text.toLowerCase());
   
-      return startDateMatch && endDateMatch && textMatch;
+      return startDateMatch && endDateMatch; //&& textMatch;
     }).sort((a, b) => {
       if (sortBy === 'date') {
         return a.createdAt < b.createdAt ? 1 : -1;
