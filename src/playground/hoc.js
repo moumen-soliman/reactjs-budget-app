0149dbbda44adeb4c942b82f0ec8e3cd03f8505a
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-25 20:44:04 +0200

    High order Component function

---
+++ src/playground/hoc.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReaactDOM from 'react-dom';
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>This info is: {props.info}</p>
+    </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>This is private info. Please don't share</p>}
+            <WrappedComponent {...props}/>
+        </div>
+    );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? (
+                <WrappedComponent {...props} />
+            ) : (
+                <p>Please login to view the info</p>
+            )}
+        </div>
+    );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+ReaactDOM.render(<Info info="alohaa2"/>, document.getElementById('app'));
